b285a0dc47e582659005d8c65d82a4437f622c4f
toko <valenservice683@gmail.com>
2020-08-27 00:44:21 -0400

    build for docs

---
+++ docs/precache-manifest.15343f5426b05dcafbc03859a0b448ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d6d5466d8f2ab1f3ddd20b03ab2b201",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e3687bc1f98f58fd8554",
+    "url": "/static/css/main.52a937d1.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3687bc1f98f58fd8554",
+    "url": "/static/js/main.c7c7f865.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
